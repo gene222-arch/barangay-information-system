620c532a8c00b575273871040e45909a0f9166f7
gene222-arch <genephillip222@gmail.com>
2022-05-08 01:33:34 +0800

    feat: update sidebar and app blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
         <nav class="navbar navbar-expand-xl navbar-dark shadow-sm app-layout-navbar">
             <div class="container-fluid">
                 <a class="navbar-brand" href="{{ url('/home-page') }}">
-                    <img class="img img-responsive" src="{{ asset("storage/app/logo.png") }}" width="40" height="40">
+                    <img class="img img-responsive" src="{{ asset("logo.png") }}" width="40" height="40">
                     <strong>Barangay Lingga IS</strong>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
